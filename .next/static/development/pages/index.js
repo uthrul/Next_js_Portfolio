8cd404d691cf596087de8d36c229fd2fc0b10175
saud maruli <uthrul@gmail.com>
2019-03-12 13:55:46 +0700

    first Project NextJs

---
+++ .next/static/development/pages/index.js
@@ -0,0 +1,89 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "../../../../node_modules/react/index.js":
+/*!*****************************************************************************************************!*\
+  !*** delegated ../../../../node_modules/react/index.js from dll-reference dll_7d44bc5b6a50749ece98 ***!
+  \*****************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_7d44bc5b6a50749ece98 */ "dll-reference dll_7d44bc5b6a50749ece98"))("../../../../node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fsauthrully%2FDesktop%2Fproject%2FProjectNext%2Fportfolio-saudMaruli%2Fpages%2Findex.js!./":
+/*!**************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fsauthrully%2FDesktop%2Fproject%2FProjectNext%2Fportfolio-saudMaruli%2Fpages%2Findex.js ***!
+  \**************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var page = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.js */ "./pages/index.js", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+          next.router.update("/", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../../../../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/sauthrully/Desktop/project/ProjectNext/portfolio-saudMaruli/pages/index.js";
+
+
+function Home() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "Welcome to next.js!");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ }),
+
+/***/ 1:
+/*!******************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fsauthrully%2FDesktop%2Fproject%2FProjectNext%2Fportfolio-saudMaruli%2Fpages%2Findex.js ***!
+  \******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fsauthrully%2FDesktop%2Fproject%2FProjectNext%2Fportfolio-saudMaruli%2Fpages%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fsauthrully%2FDesktop%2Fproject%2FProjectNext%2Fportfolio-saudMaruli%2Fpages%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_7d44bc5b6a50749ece98":
+/*!*******************************************!*\
+  !*** external "dll_7d44bc5b6a50749ece98" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_7d44bc5b6a50749ece98;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=index.js.map
