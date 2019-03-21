1e349dcd6974884969ccfa4e0cedf7f89c7a0d59
saud maruli <uthrul@gmail.com>
2019-03-21 14:33:32 +0700

    styling css and add images

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Header from '../shared/Header'
 
 const BaseLayout = (props) => {
+    const {className, children } = props;
     return (
-        <React.Fragment>
-            <Header />
-            {props.children}
-        </React.Fragment>
+        <div className="layout-container">
+        <Header/>
+        <main className={`cover ${className}`}>
+            <div className="wrapper">
+                {children}
+            </div>
+        </main>
+        </div>
     )
 }
 export default BaseLayout;
